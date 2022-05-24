8bdecc5ddfb5055ba1dd36ed71b149e78309f480
web.bunny96 <web.bunny96@gmail.com>
2022-05-24 22:43:42 +0300

    feat: Create

--- lang/lang.js
+++ lang/lang.js
@@ -1,73 +1,73 @@
 
 
 let ukrNav = {
-    "aboutMe": "Про мене",
-    "eduction": "Освіта",
-    "works": "Роботи",
-    "contacts": "Контакти",
-    "price": "Прайс"
+    aboutMe: "Про мене",
+    eduction: "Освіта",
+    works: "Роботи",
+    contacts: "Контакти",
+    price: "Прайс"
 }
 
 let ukrStory = {
-    "h2": "Front-End Розробник",
-    "p": 'Мене звуть Максим. Займаюся розробкою та програмуванням понад 5 років. За весь цей час виробив уміння швидко та якісно писати код, а знання великої кількості бібліотек дає мені можливість написання багатофункціональних та оптимізованих додатків та сервісів, які зручні та прості у використанні, що привабить велику кількість людей до вашої продукції.'
+    h2: "Front-End Розробник",
+    p: 'Мене звуть Максим. Займаюся розробкою та програмуванням понад 5 років. За весь цей час виробив уміння швидко та якісно писати код, а знання великої кількості бібліотек дає мені можливість написання багатофункціональних та оптимізованих додатків та сервісів, які зручні та прості у використанні, що привабить велику кількість людей до вашої продукції.'
 }
 
 let ukrSoftSkil = {
-    "timeManagment":"Time managment",
-    "continuousLearning":"Continuous learning",
-    "emotionalIntelligence":"Emotional intelligence",
-    "adaptability":"Adaptability",
-    "collaborationTeamwork":"Collaboration & Teamwork",
-    "resilienceCuriosity":"Resilience, Curiosity",
-    "coachingMindset":"Coaching Mindset",
-    "projectManagement":"Project Management",
-    "crativity":"Crativity",
-    "criticalThinking":"Critical Thinking & Problem Solving"
+    timeManagment:"Time managment",
+    continuousLearning:"Continuous learning",
+    emotionalIntelligence:"Emotional intelligence",
+    adaptability:"Adaptability",
+    collaborationTeamwork:"Collaboration & Teamwork",
+    resilienceCuriosity:"Resilience, Curiosity",
+    coachingMindset:"Coaching Mindset",
+    projectManagement:"Project Management",
+    crativity:"Crativity",
+    criticalThinking:"Critical Thinking & Problem Solving"
 }
 
 let ukrWorks = {
-    "works": "Роботи"
+    works: "Роботи"
 }
 
 let ukrCopyright = {
-    "first":"Усі права захищені 2022",
-    "second":"Усі права захищені 2022"
+    first:"Усі права захищені 2022",
+    second:"Усі права захищені 2022"
 }
 
 let enNav = {
-    "aboutMe": "About me",
-    "eduction": "Eduction",
-    "works": "Works",
-    "contacts": "Contacts",
-    "price": "Price"
+    aboutMe: "About me",
+    eduction: "Eduction",
+    works: "Works",
+    contacts: "Contacts",
+    price: "Price"
 }
 
 let enStory = {
-    "h2": "Front-End Developer",
-    "p": 'My name is Max. I have been developing and programming for over 5 years. During all this time, I have developed the ability to write code quickly and efficiently, and the knowledge of a large number of libraries gives me the opportunity to write multifunctional and optimized applications and services that are convenient and easy to use, which will attract a large number of people to your products.'
+    h2: "Front-End Developer",
+    p: 'My name is Max. I have been developing and programming for over 5 years. During all this time, I have developed the ability to write code quickly and efficiently, and the knowledge of a large number of libraries gives me the opportunity to write multifunctional and optimized applications and services that are convenient and easy to use, which will attract a large number of people to your products.'
 }
 
 let enSoftSkil = {
-    "timeManagment":"Time managment",
-    "continuousLearning":"Continuous learning",
-    "emotionalIntelligence":"Emotional intelligence",
-    "adaptability":"Adaptability",
-    "collaborationTeamwork":"Collaboration & Teamwork",
-    "resilienceCuriosity":"Resilience, Curiosity",
-    "coachingMindset":"Coaching Mindset",
-    "projectManagement":"Project Management",
-    "crativity":"Crativity",
-    "criticalThinking":"Critical Thinking & Problem Solving"
+    timeManagment:"Time managment",
+    continuousLearning:"Continuous learning",
+    emotionalIntelligence:"Emotional intelligence",
+    adaptability:"Adaptability",
+    collaborationTeamwork:"Collaboration & Teamwork",
+    resilienceCuriosity:"Resilience, Curiosity",
+    coachingMindset:"Coaching Mindset",
+    projectManagement:"Project Management",
+    crativity:"Crativity",
+    criticalThinking:"Critical Thinking & Problem Solving"
 }
 
 let enWorks = {
-    "works": "Works"
+    works: "Works"
 }
 
 let enCopyright = {
-    "first":"All rights reserved 2022",
-    "second":"All rights reserved 2022"
+    first:"All rights reserved 2022",
+    second:"All rights reserved 2022"
 }
 
 
